da254f3045d78f41c0f8b1735e71a155034df587
Sergiu Matei <sergiu.matei@ly.st>
2023-09-19 00:38:53 +0300

    Add Procfile and dependencies

--- ballermoRezervations/settings.py
+++ ballermoRezervations/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-wj)7@@%v7-%016=jk@)-6+d2_%#64jgc=1$ap&#h&&x6c9w$l6
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -116,6 +116,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATIC_ROOT = BASE_DIR / "staticfiles"
+
+STATICFILES_DIRS = [BASE_DIR / "static"]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
