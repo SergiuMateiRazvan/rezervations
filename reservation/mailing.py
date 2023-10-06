ff26c607bba78a11a896f87af2794a963eec0e5c
Sergiu Matei <sergiu.matei@ly.st>
2023-10-06 16:45:15 +0300

    Update restaurant name

--- reservation/mailing.py
+++ reservation/mailing.py
@@ -4,10 +4,10 @@ from django.core.mail import mail_admins, send_mail
 
 def mail_customer(reservation, confirmed=True):
     if confirmed:
-        subject = "Your Reservation at Ballermo confirmed"
+        subject = "Your Reservation at Bellari confirmed"
         message = "Salut"
     else:
-        subject = "Your Reservation at Ballermo NOT confirmed"
+        subject = "Your Reservation at Bellari NOT confirmed"
         message = ""
 
     success = send_mail(
